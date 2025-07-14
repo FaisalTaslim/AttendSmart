2d962884c7416ef268d4d76783aaa2ed0518ac3c
famaeoxiaa <faisaltaslim79@gmail.com>
2025-07-14 11:15:20 +0530

    Created desktop layout for school student dashboard

---
+++ Dashboards/studentDashboard/schoolStudent/toggleNotice.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showNoticesBtn = document.getElementById("showNoticesBtn");
+    const showLeaveBtn = document.getElementById("showLeaveBtn");
+
+    const notices = document.querySelector(".notices");
+    const requestLeave = document.querySelector(".request-leave");
+
+    notices.style.display = "none";
+    requestLeave.style.display = "block";
+
+    showNoticesBtn.addEventListener("click", () => {
+        notices.style.display = "block";
+        requestLeave.style.display = "none";
+    });
+
+    showLeaveBtn.addEventListener("click", () => {
+        notices.style.display = "none";
+        requestLeave.style.display = "block";
+    });
+});
