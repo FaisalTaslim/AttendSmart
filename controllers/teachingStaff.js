08b78a361eaf190b09913020ab22a96f2c6a3682
famaeoxiaa <faisaltaslim79@gmail.com>
2025-09-18 20:06:30 +0530

    Added request-leave, edit-info, and notices section for teaching Staffs

--- controllers/teachingStaff.js
+++ controllers/teachingStaff.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Employee = require('../models/Employee');
+const Notice = require('../models/notice');
+const LeaveRequest = require('../models/userLeave');
 const { EmployeeSummary } = require('../models/attendanceSummary');
 const checkRole = require('../middleware/roleMiddleware');
 
@@ -10,16 +12,28 @@ router.get('/', checkRole(['Employee']), async (req, res) => {
         const findUser = await Employee.findOne({ uniqueId: user });
 
         if (!findUser) return res.status(404).send("User not found");
+        const { org, uniqueId, employeeId, userName, dept, designation, contact, email } = findUser;
 
-        const { uniqueId, employeeId, userName, designation, contact, email } = findUser;
+        const allNotices = await Notice.find({ uniqueId: org });
+        const notices = allNotices.filter(notice => {
+            if (!notice.userIdType && !notice.userIdValue) return true;
+            if (notice.userIdType === "uniqueUserId" && notice.userIdValue === uniqueId) return true;
+            if (notice.userIdType === "rollNo" && notice.userIdValue === roll) return true;
+            return false;
+        });
+
+        const leaveRequests = await LeaveRequest.find({ uniqueId: uniqueId }).sort({ createdAt: -1 });
 
         res.render('view-dashboards/teachingStaff', {
             uniqueId,
             employeeId,
             userName,
+            dept,
             designation,
             contact,
             email,
+            notices: notices || [],
+            leaveRequests: leaveRequests || []
         });
 
     } catch (error) {
