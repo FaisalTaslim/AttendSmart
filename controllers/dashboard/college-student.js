bdbeee07bde6af751dce9856403a10ac2bc666b7
FaisalTaslim <faisaltaslim79@gmail.com>
2026-02-02 19:47:20 +0530

    Made guidepage responsive

--- controllers/dashboard/college-student.js
+++ controllers/dashboard/college-student.js
@@ -1,7 +1,7 @@
 const CollegeStudent = require('../../models/users/college-student');
 
 exports.dashboard = async (req, res) => {
-    const getStudent = await COllegeStudent.findOne({code: req.session.user.code});
+    const getStudent = await CollegeStudent.findOne({code: req.session.user.code});
     const setup = getStudent.setup_done;
 
     res.render('dashboard/college-student', {setup});
