c3d5c59fc85544d133c5f713bb1706808df8b8ac
FaisalTaslim <faisaltaslim79@gmail.com>
2026-02-24 17:07:21 +0530

    prepared the attendance-marking UI for school students

--- controllers/main/dashboard/school-student.js
+++ controllers/main/dashboard/school-student.js
@@ -3,6 +3,7 @@ const SchoolStudent = require('../../../models/users/school-student');
 exports.dashboard = async (req, res) => {
     const getStudent = await SchoolStudent.findOne({code: req.session.user.code});
     const setup = getStudent.setup.faceUploaded;
+    const subjects = Array.isArray(getStudent?.subjects) ? getStudent.subjects : [];
 
-    res.render('dashboard/school-student', {setup});
+    res.render('dashboard/school-student', {setup, subjects});
 };
