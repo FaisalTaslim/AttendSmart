9582a01f42a9fe45b80fed023f8769a83e741d84
FaisalTaslim <faisaltaslim79@gmail.com>
2026-07-04 13:37:50 +0530

    Admin registration modularized

--- controllers/registration/admin.js
+++ controllers/registration/admin.js
@@ -4,14 +4,12 @@ const crypto = require('crypto');
 
 const validateFields = require('../../utils/validate-fields');
 const generateCode = require('../../utils/generate-code');
-const lowercase = require('../../utils/lowercase');
 
 const { returnOrg } = require('../../services/fetch/users');
 const { updateAdmin } = require('../../services/update/users');
 const { registerLog } = require('../../services/create/logs');
 const { createOrg } = require('../../services/create/users');
 const { sendVerificationEmail } = require('../../services/emails/send-registration-emails');
-
 
 function verifyRequest(req, org) {
   const verify = {
@@ -37,127 +35,138 @@ function verifyRequest(req, org) {
   }
 }
 
-exports.adm = async (req, res) => {
-  const session = await mongoose.startSession();
+async function processData(req) {
+  const { organization, branch, address, type, website, attendanceMethod, termsCheck, admin, confirmPassword } = req.body;
+  const { name, adminId, email, contact } = req.body.admin[0];
+  let state = { code: null, exists: null };
 
-  const l = { code: null, data: null, verify: null, existing: null, exists: null, orgDoc: null, adminData: null, adm: null, setup: null, lowercased: null };
-
-  l.adminData = {
-    name: req.body.admin[0].name,
-    adminId: req.body.admin[0].adminId,
-    contact: req.body.admin[0].contact,
-    email: req.body.admin[0].email,
+  while (true) {
+    state.code = generateCode(6, "numeric");
+    state.exists = await returnOrg({ code: state.code });
+    if (!state.exists) break;
   }
 
-  adm = lowercase(adminData);
+  const data = {
+    code: state.code,
+    org: organization.toLowerCase().trim(),
+    branch: branch.toLowerCase().trim(),
+    address: address.toLowerCase().trim(),
+    type: type,
+    website: website.toLowerCase().trim(),
+    attendanceMethod: (type === 'corporate') ? null : attendanceMethod,
+    admin: [
+      {
+        name: name.toLowerCase().trim(),
+        adminId: adminId.toLowerCase().trim(),
+        email: email.toLowerCase().trim(),
+        contact: contact,
+        password: await bcrypt.hash(req.body.admin[0].password, 10),
+        role: 'admin',
+      }
+    ],
+    verification: {
+      status: 'pending',
+      token: crypto.randomBytes(32).toString('hex'),
+      expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    },
+    setup: {
+      subjectsUploaded: (type === 'corporate') ? true : false,
+      scheduleUploaded: false,
+      done: false,
+    }
+  }
+
+  return data;
+}
+
+exports.adm = async (req, res) => {
+  const session = mongoose.startSession();
+  await session.startTransaction();
+
+  let object = { org: null, log: null };
+  const data = await processData(req);
+  object.org = await returnOrg({ org: data.org, branch: data.branch });
 
   try {
-    l.existing = await returnOrg({ org: req.body.organization, branch: req.body.branch });
-    verifyRequest(req, l.existing);
+    if (verifyRequest(req, object.org)) {
+      if (object.org) {
+        await updateAdmin(object.org.code, data.admin[0], 'pushAdmin', session);
 
-    session.startTransaction();
+        object.log = {
+          type: 'success',
+          org: object.org.code,
+          name: data.admin[0].name,
+          role: 'admin',
+          id: data.admin[0].adminId,
+          email: data.admin[0].email,
+          contact: data.admin[0].contact,
+          message: 'Successful!!',
+          approvalStatus: null,
+        }
 
-    if (existing) {
-      if (existing.admin?.some((a) => a.email === email || a.adminId === adminId)) {
-        throw new Error('Admin already exists! Login with your ID and password');
+        await registerLog(object.log, session);
       }
+      else {
+        await updateAdmin(await data.code, data, 'normal', session);
 
-      await updateAdmin(existing.code, adm, 'pushAdmin', session);
+        object.log = {
+          type: 'success',
+          org: data.code,
+          name: data.admin[0].name,
+          id: data.admin[0].adminId,
+          role: 'admin',
+          email: data.admin[0].email,
+          contact: data.admin[0].contact,
+          message: 'Successful!!',
+          approvalStatus: null,
+        }
 
-      data = {
-        type: 'success',
-        org: l.existing.code,
-        name: adm.name,
-        role: 'admin',
-        id: adm.adminId,
-        email: adm.email,
-        contact,
-        message: 'Another admin added successfully!',
-        approvalStatus: null,
+        await registerLog(object.log, session);
+
+        await sendVerificationEmail(data.admin[0].email, (await data).verification.token, (await data).code, 'admin', null);
+        await session.commitTransaction();
       }
-
-      await registerLog(data, session);
-    }
-    else {
-      while (true) {
-        l.code = generateCode(6, "numeric");
-        l.exists = await returnOrg({ code: l.code });
-        if (!l.exists) break;
-      }
-
-      const token = crypto.randomBytes(32).toString('hex');
-
-      setup =
-        req.body.type === 'corporate'
-          ? { subjectsUploaded: true, scheduleUploaded: false, done: false }
-          : { subjectsUploaded: false, scheduleUploaded: false, done: false };
-
-      data = {
-        org: req.body.organization,
-        branch: req.body.branch,
-        address: req.body.address,
-        type: req.body.type,
-        website: req.body.website,
-        attendanceMethod: (type === 'corporate') ? null : req.body.attendanceMethod,
-        agreement: true,
-        setup: req.body.setup,
-      }
-      lowercased = lowercase(data);
-      lowercased.admin = adm;
-
-
-      const createdOrg = await createOrg(lowercased, session);
-
-      data = {
-        type: 'success',
-        org: l.code,
-        name: adm.name,
-        id: adm.adminId,
-        role: 'admin',
-        email: adm.email,
-        contact: adm.contact,
-        message: 'admin registered successfully!',
-        approvalStatus: null,
-      }
-
-      await registerLog(data, session);
-
-      await sendVerificationEmail(adm.email, token, l.code, 'admin', null);
-
-      await session.commitTransaction();
-      session.endSession();
-
-      const params = new URLSearchParams({
-        "popup-type": 'success',
-        "popup-message": 'All Set!! Verify your org! We have sent you a mail.',
-      });
-
-      return res.redirect(`/?${params}`);
     }
   } catch (err) {
     if (session.inTransaction()) {
       await session.abortTransaction();
     }
-    session.endSession();
 
-    data = {
-      type: 'failed',
-      org: l.code ?? null,
-      name: adm.name ?? null,
-      id: adm.adminId ?? null,
-      role: 'admin',
-      email: adm.email ?? null,
-      contact: adm.contact ?? null,
-      message: err.message
+    if (object.org) {
+      object.log = {
+        type: 'failed',
+        org: object.org.code ?? null,
+        name: data.admin[0].name ?? null,
+        role: 'admin',
+        id: data.admin[0].adminId ?? null,
+        email: data.admin[0].email ?? null,
+        contact: data.admin[0].contact ?? null,
+        message: 'Successful!!',
+        approvalStatus: null,
+      }
+    }
+    else {
+      object.log = {
+        type: 'failed',
+        org: data.code ?? null,
+        name: data.admin[0].name ?? null,
+        id: data.admin[0].adminId ?? null,
+        role: 'admin',
+        email: data.admin[0].email ?? null,
+        contact: data.admin[0].contact ?? null,
+        message: 'Successful!!',
+        approvalStatus: null,
+      }
     }
 
-    await registerLog(data);
-
+    await registerLog(object.log, session);
     const params = new URLSearchParams({
       "popup-type": "error",
       "popup-message": err.message || "Registration failed. Please try again.",
     });
 
     return res.redirect(`/?${params}`);
+  } finally {
+    await session.endSession();
   }
-};
+}
